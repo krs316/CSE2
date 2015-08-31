6cc7e9faa241a4b3f8a81057fe71156b8365bd08
krs316 <krs316@lehigh.edu>
2015-08-31 00:00:59 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//Kyle Sammon
+//Hello World java Program
+//define class
+public class HelloWorld{
+    //add main domain
+        public static void main(String[]args){
+//print statement
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("#                      #");
+            System.out.println("#  Hello World 2014    #");
+            System.out.println("#                      #");
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("testing");
+        }
+}
