9edb12e1bd4f6e9d56733045cdbd5d2c2d6fc5ea
krs316 <krs316@lehigh.edu>
2015-08-31 21:15:26 +0000

    homework 1 file

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//Kyle Sammon
+//8/31/15
+//CSE2
+//Welcome Class Program
+
+public class WelcomeClass{
+    //add main domain
+        public static void main(String[]args){
+    //print statement with dashes and arrows surrounding WelcomeClass, my lehigh login, and a brief bio.
+    //have to use double backslash in print statement to prevent error.
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-K--R--S--3--1--6->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("This past Spring I bought a mountian bike and have been riding at Lehigh.\nSo far I havn't broken any bones!");
+        }
+}
