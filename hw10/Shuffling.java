6f241f004ce763b631cd9140c5b9f2c0bb3eeb84
krs316 <krs316@lehigh.edu>
2015-11-03 22:00:35 +0000

    completed hw10

--- hw10/Shuffling.java
+++ hw10/Shuffling.java
@@ -1,4 +1,11 @@
-import java.util.Scanner;
+//Kyle Sammon
+//CSE2
+//hw10 Shuffling
+//11/3/15
+//finds the area of a trinagle, rectangle or circle using method
+
+
+import java.util.Scanner; //imports scanner
 public class Shuffling{
   public static void main(String[] args) {
      Scanner scan = new Scanner(System.in);
@@ -11,34 +18,32 @@ public class Shuffling{
         cards[i]=rankNames[i%13]+suitNames[i/13];
         // System.out.print(cards[i]+" ");
         }
-    //System.out.print("hello");
-    printArray(cards);
-    shuffle(cards);
-    System.out.println("\nShuffled");
-    printArray(cards);
-    hand = randomizeHand(cards);
-    printArray(hand);
+    printArray(cards); //inputs cards array to pritn array method
+    shuffle(cards); //inputs cards array to shuffle method
+    printArray(cards); //prints shuffled cards using print method
+    hand = randomizeHand(cards); //sends cards array to hand genreator which returns a hand of 5 cards
+    printArray(hand); //prints out hte random hand
   }
   public static void printArray(String[] list){
+        // prints out the array that is input into the system. Stops at correct dimension for input array
         for (int i=0; i<list.length; i++){
-        //list[i]=rankNames[i%13]+suitNames[i/13];
         System.out.print(list[i]+" ");
         }
     }
     public static void shuffle(String[] b){
         System.out.println("\nShuffled");
         int x=0;
+        //shufles the deck by randomly selcting a card and switching it with the card at index 0
         for(int j=0;j<100;j++){
         x=(int)(Math.random()*(52));
         String y=b[x];
         b[x]=b[0];
         b[0]=y;
-        //System.out.print(j);
         }
-        //System.out.print(x);
     }
     public static String[] randomizeHand(String[] c){
         System.out.println("\nHand");
+        //creates five random numbers from 0 to 51 with no two being the same
         int x1=(int)(Math.random()*(52));
         int x2=(int)(Math.random()*(52));
         while(x2==x1){
@@ -56,13 +61,15 @@ public class Shuffling{
         while(x5==x4||x5==x3||x5==x2||x5==x1){
            x5=(int)(Math.random()*(52));
         }
+        //creates ne String array to be returned
         String[] f=new String [5];
+        //sets the value for each string array to be returned for the random hand
         f[0]=c[x1];
         f[1]=c[x2];
         f[2]=c[x3];
         f[3]=c[x4];
         f[4]=c[x5];
-        return f;
+        return f; //returns the random hand
         
     }
 }
